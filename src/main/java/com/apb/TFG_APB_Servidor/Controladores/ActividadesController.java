b59097e2b74b427ede9058996bc67fe2fe3e699d
Alph4Bet4 <aperbla737@g.educaand.es>
2024-03-27 13:46:03 +0100

    Cambio de nombre al path

--- src/main/java/com/apb/TFG_APB_Servidor/Controladores/ActividadesController.java
+++ src/main/java/com/apb/TFG_APB_Servidor/Controladores/ActividadesController.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/actividades")
+@RequestMapping("/actividad")
 public class ActividadesController {
 
     @Autowired
