c5c591d784da2d73a41992bce3337a1ece01417f
Alph4Bet4 <aperbla737@g.educaand.es>
2024-03-27 14:38:03 +0100

    Añadida la anotación repositorio.

--- src/main/java/com/apb/TFG_APB_Servidor/Repositorios/ISugerenciaActividadesRepositorio.java
+++ src/main/java/com/apb/TFG_APB_Servidor/Repositorios/ISugerenciaActividadesRepositorio.java
@@ -2,6 +2,8 @@ package com.apb.TFG_APB_Servidor.Repositorios;
 
 import com.apb.TFG_APB_Servidor.Modelos.SugerenciaActividadesModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ISugerenciaActividadesRepositorio extends JpaRepository<SugerenciaActividadesModel, Integer> {
 }
